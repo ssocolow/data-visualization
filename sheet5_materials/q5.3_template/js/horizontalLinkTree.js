1cbc210677f674b8ea9bd9234a2388f6b41ed372
Simon Socolow <simonsocolow78@gmail.com>
2025-03-02 19:58:54 +0000

    done with q 5-3

--- sheet5_materials/q5.3_template/js/horizontalLinkTree.js
+++ sheet5_materials/q5.3_template/js/horizontalLinkTree.js
@@ -18,7 +18,48 @@ export const horizontalLinkTree = (parent, props) => {
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
   // Implement tree chart
-  // ...
+  const treeData = d3.hierarchy(data);
+
+  console.log(treeData);
+  const treeLayout = d3.tree()
+    .size([innerHeight, innerWidth]);
+  const links = treeLayout(treeData).links();
+  const linkPathGenerator = d3.linkHorizontal()
+    .x(d => d.y)
+    .y(d => d.x);
+  
+  chart.selectAll('.link').data(links)
+    .join('path')
+    .attr('class', 'link')
+    .attr('d', linkPathGenerator);
+  
+  const nodesG = chart.selectAll('.node-group').data(treeData.descendants())
+    .join('g')
+    .attr('transform', d => `translate(${d.y},${d.x})`);
+
+  nodesG.append('text')
+    .attr('class', 'label')
+    .attr('x', d => d.children ? 0 : 1)
+    .attr('font-size', d => 5 + (10 * d.height))
+    .attr('text-anchor', d => d.children ? 'middle' : 'start')
+    .text(d => {
+      // console.log(d);
+      return d.data.data.id;
+    });
+      
+  // Zoom interactivity (using d3-zoom package -- standard d3 bundle)
+  const zoom = d3.zoom()
+    .scaleExtent([0.5, 10])
+    .on('zoom', (event) => {
+      if (event.transform.k === 0.5) {
+        parent.transition()
+          .duration(750)
+          .call(zoom.transform, d3.zoomIdentity.translate(0, 0));
+      } else {
+        chart.attr('transform', `translate(${event.transform.x + margin.left},${event.transform.y + margin.top}) scale(${event.transform.k})`);
+      }
+    });
+
+  parent.call(zoom);  // Apply zoom to the parent SVG, not the chart group
 
 };
-
--- sheet5_materials/q5.3_template/js/index.js
+++ sheet5_materials/q5.3_template/js/index.js
@@ -17,4 +17,3 @@ d3.json('./data/data.json')
   
   });
 
-
